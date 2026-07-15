2ebf9fce1d0d5d60914f580330e9eea0bb4669b5
Tanu Mangal <tanu.mangal_cs24@gla.ac.in>
2026-07-15 14:09:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,112 @@
+//age=2;
+//fullname = "tanu"
+//number , albhabet ,_ and dolllar other
+
+// abc @ = 4;
+// abc @c = 5;
+
+
+// fullmame = 45;
+// full_name$12 = 45;
+// _full_name$12 = "tanu";
+// fullname"tanu";
+
+
+//variable declaration
+// can be decalare and update
+// var age = 2;
+// age = 40;
+// //fullname = age;
+// console.log("age is a=".);
+
+// //can't be redeclare but can update
+// let age = 20;
+// age = 40;
+// console.log("age is a =",age);
+
+// // cnat't be redeclare and can't update
+
+// const pi = 3.14,
+
+//data types
+
+// 1.. string
+//" " use krte h likhne me,characters hote h,
+// let fullname="tanu mangal";
+// full name = "tanu";
+// age = 20.1542
+
+
+//console.log("hello world");
+//loops
+//inialization
+//increamemt and decrement
+//condition check
+
+// for loop(inialization ; condition;increment/decrement){
+//set of statement
+//}
+// for(i=1;i<=10;i++){
+//     console.log(i);
+//     console.log("hello");
+// }
+
+
+// array
+// let student1="abhishek";
+// let student2="tanu";
+// let student3="mangal";
+// let student4="suman";
+// let student5="sita";
+ //let students = ["abhishek","tanu","mangal","suman","sita"];
+// let rollno = [1,2,3,4,5];
+// console.log(students[0]);
+// console.log(students.length);
+// students.push("ram")//add element
+// students.unshift("shyam");//shift in first element
+// students.pop();//remove element
+// //iterate array by help of loop
+//let number=[1,2,3,4,5,6,7,8,9,10];
+
+// for(let i=0;i<students.length;i++){
+//     if(number[i]%2==0){
+//         console.log(`${number[i]} is even number`)
+//     }
+// }
+
+
+//array
+//array is a collection of data
+//array is a data structure which can store multiple values in a single variable
+//array is a collection of data which can store multiple values in a single variable
+
+
+// for of loop
+// for(let number of numbers){
+//     if(number %2==0){
+//         console.log(`${number}is even number`);
+//         continue;
+//         //loop ko discountiue -break
+//         //continue-
+//     }
+//     console.log(number[i]);//continue means skipppp..............
+
+// }
+
+// for(let student of students){
+//     console.log(students);
+
+// }
+
+// // for in
+// for(let i in number){// in ka mtlb index
+//     // of means value
+//     console.log(i);
+// }
+// function
+
+//function are resuable block of code which can be called multiple times 
+
+//dom manipulation
+
+
