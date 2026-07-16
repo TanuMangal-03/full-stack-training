2d510af15e102306c79a5f74e708b751b97f7c80
Tanu Mangal <tanu.mangal_cs24@gla.ac.in>
2026-07-16 14:10:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,58 @@
-//age=2;
-//fullname = "tanu"
-//number , albhabet ,_ and dolllar other
 
-// abc @ = 4;
-// abc @c = 5;
+  
+// document.getElementById("btn").onclick = function(){
+// document.getElementById("title").textContent = "welcome to javascript";  
 
+//};
 
-// fullmame = 45;
-// full_name$12 = 45;
-// _full_name$12 = "tanu";
-// fullname"tanu";
+// const boxes = document.querySelectorAll('.box');
+// boxes.forEach(box => {
+//  box.oncliclk = function() {
+//    box.computedStyleMap.backgroundColor = 'blue';
+//  };
+// }); 
+// console.log(boxes);
 
+// const box = document.querySelector('.box');
+// // box.style.height = "100px";
+// // box.style.width = " 100px";
+// // box.style.backgroundcolor = "red";
 
-//variable declaration
-// can be decalare and update
-// var age = 2;
-// age = 40;
-// //fullname = age;
-// console.log("age is a=".);
-
-// //can't be redeclare but can update
-// let age = 20;
-// age = 40;
-// console.log("age is a =",age);
-
-// // cnat't be redeclare and can't update
-
-// const pi = 3.14,
-
-//data types
-
-// 1.. string
-//" " use krte h likhne me,characters hote h,
-// let fullname="tanu mangal";
-// full name = "tanu";
-// age = 20.1542
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("click", function(event) {
+//    console.log("hello");
+// box.style.height = "400px";
+// box.style.width = " 400px";
+// box.style.backgroundcolor = "blue";
+  
+  
+// });
 
 
-//console.log("hello world");
-//loops
-//inialization
-//increamemt and decrement
-//condition check
-
-// for loop(inialization ; condition;increment/decrement){
-//set of statement
-//}
-// for(i=1;i<=10;i++){
-//     console.log(i);
-//     console.log("hello");
-// }
 
 
-// array
-// let student1="abhishek";
-// let student2="tanu";
-// let student3="mangal";
-// let student4="suman";
-// let student5="sita";
- //let students = ["abhishek","tanu","mangal","suman","sita"];
-// let rollno = [1,2,3,4,5];
-// console.log(students[0]);
-// console.log(students.length);
-// students.push("ram")//add element
-// students.unshift("shyam");//shift in first element
-// students.pop();//remove element
-// //iterate array by help of loop
-//let number=[1,2,3,4,5,6,7,8,9,10];
+// const box = document.querySelector('.box');
+// const btn = document.querySelector('.btn');
 
-// for(let i=0;i<students.length;i++){
-//     if(number[i]%2==0){
-//         console.log(`${number[i]} is even number`)
-//     }
-// }
+// btn.addEventListener('click', function() {
+//  box.classList.add("big-box");
+//  box.classList.remove("box");
+// });
 
 
-//array
-//array is a collection of data
-//array is a data structure which can store multiple values in a single variable
-//array is a collection of data which can store multiple values in a single variable
+let isSmall = true;
+const box = document.querySelector('.box');
+const btn = document.querySelector('.btn');
 
-
-// for of loop
-// for(let number of numbers){
-//     if(number %2==0){
-//         console.log(`${number}is even number`);
-//         continue;
-//         //loop ko discountiue -break
-//         //continue-
-//     }
-//     console.log(number[i]);//continue means skipppp..............
-
-// }
-
-// for(let student of students){
-//     console.log(students);
-
-// }
-
-// // for in
-// for(let i in number){// in ka mtlb index
-//     // of means value
-//     console.log(i);
-// }
-// function
-
-//function are resuable block of code which can be called multiple times 
-
-//dom manipulation
-
-
+btn.addEventListener('click', function() {
+ if (isSmall == true ) {
+    box.classList.add("big-box");       
+    box.classList.remove("box");
+    isSmall = false;
+ } else {
+    box.classList.add("box");       
+    box.classList.remove("big-box");
+    isSmall = true;
+ }
+});
+   
